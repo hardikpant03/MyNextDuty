4b887e9eed9bac5abb6ac8f97dd8b815f385d98e
MohitPimoli <mohitpimoli31@gmail.com>
2025-12-03 23:04:52 +0530

    add: route config

---
+++ lens/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PrivateRouteConfig } from "./routes/RouteConfig";
+
+export default function App() {
+  return <h1 style={{ color: "red" }}>Hello React</h1>;
+}
